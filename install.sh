a47819fa1bb9ee757e095cf851bdb5b16c0e77fb
reprereer <86232139+Reprereer@users.noreply.github.com>
2023-07-11 02:26:51 +0900

    temp_jarpathを定義した

--- install.sh
+++ install.sh
@@ -33,8 +33,10 @@ function stopServer() {
 
 function downloadFile() {
 	echo "Downloading Minecraft Forge Server ..."
+ 	url = "https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.2-43.2.0/forge-1.19.2-43.2.0-installer.jar"
+ 	temp_jarpath="/tmp/$(basename ${url})"
  
-    if ! wget -q -O ${temp_jarpath} 'https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.2-43.2.0/forge-1.19.2-43.2.0-installer.jar'; then
+    if ! wget -q -O ${temp_jarpath} ${url}; then
         echo "ERROR: Cannot download file: ${url}"
         exit 1
     fi
